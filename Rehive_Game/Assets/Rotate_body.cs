24b77edd9c2519c1876e063c0532d4738f00a338
tjepster <jeppevidw@gmail.com>
2020-03-03 15:53:42 +0100

    rotation
    arg

---
+++ Rehive_Game/Assets/Rotate_body.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_body : MonoBehaviour
+{
+    public float turnspeed = 10f;
+    void Update()
+    {
+        if (Input.GetKey("f"))
+        {
+            transform.Rotate(Vector3.back, turnspeed * Time.deltaTime);
+        }
+        if (Input.GetKey("g"))
+        {
+            transform.Rotate(-Vector3.back, turnspeed * Time.deltaTime);
+        }
+    }
+}
