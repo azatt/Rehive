71feed650138b3e73fd311de9280568aea1666e5
Stefan <stefan13401@outlook.com>
2020-03-13 18:05:25 +0100

    Merge branch 'UI/All3'
    see scenes mainmenu and optionsmenu

---
+++ Rehive_Game/Assets/UI/AudioVolume.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+
+public class AudioVolume : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetMusicVolume(float sliderValue)
+    {
+        audioMixer.SetFloat("MusicVolumeSlave", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void SetSFXVolume(float sliderValue)
+    {
+        audioMixer.SetFloat("SFXVolumeSlave", Mathf.Log10(sliderValue) * 20);
+    }
+}
