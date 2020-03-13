53120e7510c9f86f997f0a3ec9ba60424207ecdb
Wietse Heijkers <wietseheijkers280@gmail.com>
2020-03-13 21:00:11 +0100

    threat meter ui
    threat meter ui

---
+++ Rehive_Game/Assets/DangerBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DangerBar : MonoBehaviour
+{
+    private Transform birdSprite, barSprite;
+
+    private void Start()
+    {
+        birdSprite = transform.Find("BirdSprite");
+        barSprite = birdSprite.Find("Container_Bar");
+        barSprite.localScale = new Vector3(1f, 0.0001f);
+    }
+
+    public void SetBarSize (float barSize)
+    {
+        barSprite.localScale = new Vector3(1f, barSize);
+    }
+    
+    
+}
---
+++ Rehive_Game/Assets/TestScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    public void GetSliderValue (float sliderValue)
+    {
+        //Debug.Log(sliderValue);
+        
+        if (sliderValue > 0.5f)
+        {
+            Debug.Log("boven de helft");
+        }
+        if (sliderValue > 0.7f)
+        { 
+            Debug.Log("boven 0.7");
+        }
+    }
+  
+}
