d902e36739d37a3bab1670c3fd84b2ca1ea436f5
Wietse Heijkers <wietseheijkers280@gmail.com>
2020-03-17 14:01:36 +0100

    power up ui
    power up ui

---
+++ Rehive_Game/Assets/SpeedMeter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedMeter : MonoBehaviour
+{
+    private Transform speedContainer, clippingMask;
+
+    private void Start()
+    {
+        clippingMask = transform.Find("ClippingMask");
+        speedContainer = clippingMask.Find("SpeedContainer");
+        speedContainer.localScale = new Vector3(0.0001f, 1f);
+    }
+
+    public void SetBarSize(float speedbarSize)
+    {
+        speedContainer.localScale = new Vector3(speedbarSize, 1f);
+    }
+}
