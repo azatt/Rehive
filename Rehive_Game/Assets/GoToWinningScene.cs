4ab24323a22586ee86abc8772a7812e090fd0a8a
Azat <azatsa@gmail.com>
2020-03-18 16:25:26 +0100

    changed final scene

--- Rehive_Game/Assets/GoToWinningScene.cs
+++ Rehive_Game/Assets/GoToWinningScene.cs
@@ -1,18 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GoToWinningScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public CanvasGroup canvasGroup;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+
+        SceneManager.LoadScene("GameWin");
     }
 }
