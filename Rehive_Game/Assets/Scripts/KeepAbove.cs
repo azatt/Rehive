6d81666142bd1404ac1022e8b4eb28ac8aff4a0f
MaikVink <maikvink@hotmail.com>
2020-03-18 20:44:22 +0100

    scripts improved

--- Rehive_Game/Assets/Scripts/KeepAbove.cs
+++ Rehive_Game/Assets/Scripts/KeepAbove.cs
@@ -8,13 +8,6 @@ public class KeepAbove: MonoBehaviour
     public float maxRayDist = 0.1f;
     public GameObject intersected;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         
--- Rehive_Game/Assets/Scripts/PowerUp.cs
+++ Rehive_Game/Assets/Scripts/PowerUp.cs
@@ -4,22 +4,11 @@ using UnityEngine;
 
 public class PowerUp : MonoBehaviour
 {
-    // Start is called before the first frame update
     public class Tag
     {
         public const string Speed = "speedPU";
         public const string Size = "sizePU";
         public const string Camo = "camoPU";
         public const string SafeZone = "SafeZone";
-    }
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
