c37e7d18a7bd20b4d1afe18ce0b89a39da5c7797
Azat <azatsa@gmail.com>
2020-03-18 16:30:33 +0100

    Changed script for finalscene

--- Rehive_Game/Assets/GoToWinningScene.cs
+++ Rehive_Game/Assets/GoToWinningScene.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,7 +10,22 @@ public class GoToWinningScene : MonoBehaviour
 
     private void Awake()
     {
+        canvasGroup.DOFade(1, 2f).SetDelay(1);
+        GoToWinning();
+    }
 
+    IEnumerator LoadWinning()
+    {
+        yield return new WaitForSeconds(3);
         SceneManager.LoadScene("GameWin");
+    }
+
+    void GoToWinning()
+    {
+        StatsController.globalThreatLevel = 0;
+        StatsController.speedValue = 0;
+        StatsController.camoValue = 0;
+        StatsController.sizeValue = 0;
+        StartCoroutine(LoadWinning());
     }
 }
